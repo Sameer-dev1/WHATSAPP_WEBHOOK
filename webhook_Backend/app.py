5830eac0e05848b2a3201ebb837b147c299854dc
Sameer Kumar Singh <sameerkumarsingh56@gmail.com>
2025-08-09 10:32:56 +0530

    added host

--- webhook_Backend/app.py
+++ webhook_Backend/app.py
@@ -91,4 +91,4 @@ def send_message():
         return jsonify({"error": f"Failed to save message: {str(e)}"}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host='0.0.0.0', port=5000)
